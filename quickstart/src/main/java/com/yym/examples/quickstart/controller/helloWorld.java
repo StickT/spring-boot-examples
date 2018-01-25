0592fa0d5cfd55d94384cdf8b21c466f4d318276
姚益民 <yaoyimin.benlai.com>
2018-01-25 22:38:28 +0800

    test

--- quickstart/src/main/java/com/yym/examples/quickstart/controller/helloWorld.java
+++ quickstart/src/main/java/com/yym/examples/quickstart/controller/helloWorld.java
@@ -1,5 +1,6 @@
 package com.yym.examples.quickstart.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +14,10 @@ public class helloWorld {
 	@RequestMapping(value = "/",method = RequestMethod.GET)
 	public String helloWorld(){
 		return "Hello World!";
+	}
+
+	@RequestMapping(value = "/hello/{param}",method = RequestMethod.GET)
+	public String helloParam(@PathVariable String param){
+		return param;
 	}
 }
